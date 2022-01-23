cff1e9808f06d23a733654100af09996454ad8f4
Nick McLarty <nick@mclarty.me>
2022-01-23 12:26:09 -0600

    Adjust shell script to pull from repo's content XML

--- honkit/catalog/update-oscal.sh
+++ honkit/catalog/update-oscal.sh
@@ -1,7 +1,8 @@
 rm TAMUS_2.0_resolved-profile_catalog.xml
-cp ~/OSCAL/content/tamus.edu/xml/TAMUS_2.0_resolved-profile_catalog.xml ./catalog.tmp
+cp ../../content/tamus.edu/xml/TAMUS_2.0_resolved-profile_catalog.xml ./catalog.tmp
 sed -i '' 's/<insert type="param" id-ref="\([a-z0-9._-]*\)"\/>/_[Assignment: {\1}]_/g' catalog.tmp
 sed -i '' 's/{\([a-z0-9_-]*\)\.\([a-z0-9_-]*\)}/{\1-\2}/g' catalog.tmp
 iconv -f utf8 -t ascii//translit catalog.tmp > TAMUS_2.0_resolved-profile_catalog.xml
 rm catalog.tmp
 python3 oscal-to-adoc.py
+rm TAMUS_2.0_resolved-profile_catalog.xml

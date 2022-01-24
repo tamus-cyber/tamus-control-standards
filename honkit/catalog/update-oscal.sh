7d16610367db50b2c9223d3634738113ba8a0555
Nick McLarty <nick@mclarty.me>
2022-01-24 15:48:11 -0600

    Move Assignment tag from sed to Python script

--- honkit/catalog/update-oscal.sh
+++ honkit/catalog/update-oscal.sh
@@ -1,6 +1,6 @@
 rm TAMUS_2.0_resolved-profile_catalog.xml
 cp ../../content/tamus.edu/xml/TAMUS_2.0_resolved-profile_catalog.xml ./catalog.tmp
-sed -i '' 's/<insert type="param" id-ref="\([a-z0-9._-]*\)"\/>/_[Assignment: {\1}]_/g' catalog.tmp
+sed -i '' 's/<insert type="param" id-ref="\([a-z0-9._-]*\)"\/>/_[{\1}]_/g' catalog.tmp
 sed -i '' 's/{\([a-z0-9_-]*\)\.\([a-z0-9_-]*\)}/{\1-\2}/g' catalog.tmp
 iconv -f utf8 -t ascii//translit catalog.tmp > TAMUS_2.0_resolved-profile_catalog.xml
 rm catalog.tmp

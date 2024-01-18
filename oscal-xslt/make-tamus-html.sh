549c8fa1955e9b7bd8ee86b6213a470dafb56339
Nick McLarty <nmclarty@cyber.tamus.edu>
2024-01-18 10:32:30 -0600

    Rename catalog from cyber control standards to security control standards

--- oscal-xslt/make-tamus-html.sh
+++ oscal-xslt/make-tamus-html.sh
@@ -17,7 +17,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 sh mvn-make-catalog-html.sh ../../tamus-control-standards/content/tamus.edu/TAMUS_resolved-profile_catalog.xml temp css-path=/dev/null
 
-{ echo '---\nlayout: catalog\ntitle: Cybersecurity Control Standards Catalog\ncustom-css: catalog.css\nredirect_from:\n  - /catalog/ac\n  - /catalog/at\n  - /catalog/au\n  - /catalog/ca\n  - /catalog/cm\n  - /catalog/cp\n  - /catalog/ia\n  - /catalog/ir\n  - /catalog/ma\n  - /catalog/mp\n  - /catalog/pe\n  - /catalog/pl\n  - /catalog/pm\n  - /catalog/ps\n  - /catalog/pt\n  - /catalog/ra\n  - /catalog/sa\n  - /catalog/sc\n  - /catalog/si\n  - /catalog/sr\n---\n'; cat temp; } > catalog.html
+{ echo '---\nlayout: catalog\ntitle: Security Control Standards Catalog\ncustom-css: catalog.css\nredirect_from:\n  - /catalog/ac\n  - /catalog/at\n  - /catalog/au\n  - /catalog/ca\n  - /catalog/cm\n  - /catalog/cp\n  - /catalog/ia\n  - /catalog/ir\n  - /catalog/ma\n  - /catalog/mp\n  - /catalog/pe\n  - /catalog/pl\n  - /catalog/pm\n  - /catalog/ps\n  - /catalog/pt\n  - /catalog/ra\n  - /catalog/sa\n  - /catalog/sc\n  - /catalog/si\n  - /catalog/sr\n---\n'; cat temp; } > catalog.html
 
 rm temp
 
@@ -31,7 +31,7 @@ mv catalog.html ../../tamus-control-standards/docs/catalog/index.html
 
 sh mvn-make-required-controls-html.sh ../../tamus-control-standards/content/tamus.edu/TAMUS_resolved-profile_catalog.xml temp css-path=/dev/null
 
-{ echo '---\nlayout: full-width\ntitle: Required Control Standards Listing\ncustom-css: required-controls.css\nredirect_from:\n  - /catalog/new-controls\n---\n'; cat temp; } > required-controls.html
+{ echo '---\nlayout: full-width\ntitle: Required Security Controls Listing\ncustom-css: required-controls.css\nredirect_from:\n  - /catalog/new-controls\n---\n'; cat temp; } > required-controls.html
 
 rm temp
 

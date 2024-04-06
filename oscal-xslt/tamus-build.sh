ed7ff307f7762c414b779cf5d8e2f214de9717fa
Nick McLarty <nmclarty@cyber.tamus.edu>
2024-04-05 21:21:05 -0500

    Update XSLT transforms for TAMUS catalog

--- oscal-xslt/tamus-build.sh
+++ oscal-xslt/tamus-build.sh
@@ -9,4 +9,3 @@ cp ../tamus-control-standards/oscal-xslt/publish/tamus-required-controls-html.xs
 
 patch publish/nist-emulation/sp800-53A-catalog_html.xsl ../tamus-control-standards/oscal-xslt/publish/nist-emulation/sp800-53A-catalog_html.xsl.patch
 patch publish/generic-preview/oscal_catalog_html.xsl ../tamus-control-standards/oscal-xslt/publish/generic-preview/oscal_catalog_html.xsl.patch
-patch publish/generic-preview/oscal_metadata_html.xsl ../tamus-control-standards/oscal-xslt/publish/generic-preview/oscal_metadata_html.xsl.patch

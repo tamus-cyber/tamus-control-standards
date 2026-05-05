6cfde98442653a357a5082b1942b7b3a90325cf3
Nick McLarty <1228609+mclarty@users.noreply.github.com>
2026-05-05 08:10:16 -0400

    Add TAMUS implementation for PM-15

--- utils/build_catalog.sh
+++ utils/build_catalog.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+source ./venv/bin/activate
 cd ../content/tamus.edu/
 oscal-cli profile resolve --overwrite --to=yaml TAMUS_profile.yaml TAMUS_resolved-profile_catalog.yaml
-source ~/venv/bin/activate
 python3 ../../utils/merge_profile_back_matter.py ../texas.gov/TX_DIR_profile.yaml TAMUS_resolved-profile_catalog.yaml --inplace
 python3 ../../utils/merge_profile_back_matter.py TAMUS_profile.yaml TAMUS_resolved-profile_catalog.yaml --inplace
-deactivate
 cd ../../utils/
+deactivate
